ac5e1cdb4e3a7eb7e85cf468c796a397fcd5cb57
Marcus Lachmanez <malachma@microsoft.com>
2021-06-14 17:23:46 +0200

    grub2-mkconfig not called for version 7.x of RedHat

--- src/linux/common/helpers/alar/alar-fki/kernel.sh
+++ src/linux/common/helpers/alar/alar-fki/kernel.sh
@@ -22,6 +22,10 @@ if [[ $isRedHat == "true" ]]; then
                 set_grub_default
                 grubby --set-default=1 # This is the previous kernel
                 
+                if [[ $(grep -qe 'VERSION_ID=\"7.\?[1-9]\?\"' /etc/os-release) ]]; then
+                        grub2-mkconfig -o /boot/grub2/grub.cfg
+                fi
+                
                 # Exception for RedHat 8.0 i.e sku RedHat:RHEL-HA:8.0:8.0.2020021914
                 # here we don't have to run the patch operation
                 if [[ $(grep -qe 'VERSION_ID="8\.0"' /etc/os-release) -eq 0 ]]; then
