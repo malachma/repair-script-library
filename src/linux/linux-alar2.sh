0a621af856c102637f393701ebc114051d01c768
malachma <malachma@microsoft.com>
2023-02-20 11:10:22 +0100

    altered the URL to use the cli-test branch

--- src/linux/linux-alar2.sh
+++ src/linux/linux-alar2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/Azure/ALAR/main/src/run-alar.sh
+wget https://raw.githubusercontent.com/Azure/ALAR/cli-test/src/run-alar.sh
 chmod 700 run-alar.sh
 ./run-alar.sh $1
 
