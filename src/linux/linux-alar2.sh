2ec13aa765ae48a5269e005d2ed07d82aa061752
Marcus Lachmanez <malachma@microsoft.com>
2024-10-18 14:47:24 +0200

    Update linux-alar2.sh
    Modified to test the alar2 redesign with vm-repair-run

--- src/linux/linux-alar2.sh
+++ src/linux/linux-alar2.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/Azure/ALAR/main/src/run-alar.sh
-chmod 700 run-alar.sh
-./run-alar.sh $@
+
+#wget https://raw.githubusercontent.com/Azure/ALAR/main/src/run-alar.sh
+#chmod 700 run-alar.sh
+#./run-alar.sh $@
+# Above is the original content, modified to run a test
+
+#/bin/bash
+
+curl -L https://github.com/Azure/ALAR/releases/download/v0.7.0-ALPHA/alar2 --output /tmp/alar2
+cd /tmp
+chmod 700 alar2
+RUST_LOG=debug ./alar2 $@
 
 exit $?
